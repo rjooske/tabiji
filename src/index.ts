f5ac9a031a211c3cf65ae0eba25fd0a92c66c647
Ryosuke Takahashi <rjooske@gmail.com>
2023-03-06 18:28:27 +0900

    Test line webhook locally (with ngrok)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { Translate } from "@google-cloud/translate/build/src/v2/index.js";
+import line from "@line/bot-sdk";
+import express from "express";
 import { writeFile } from "fs/promises";
 import fetch from "node-fetch";
 import { basename } from "path";
@@ -44,6 +46,44 @@ const stringToUint16 = z
 async function main() {
   const secrets = Secrets.parse(process.env);
   const port = stringToUint16.parse(process.env.PORT);
+
+  const lineClient = new line.Client({
+    channelSecret: secrets.LINE_CHANNEL_SECRET,
+    channelAccessToken: secrets.LINE_CHANNEL_ACCESS_TOKEN,
+  });
+
+  const app = express();
+
+  app.post(
+    "/webhook",
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    line.middleware({
+      channelSecret: secrets.LINE_CHANNEL_SECRET,
+      channelAccessToken: secrets.LINE_CHANNEL_ACCESS_TOKEN,
+    }),
+    (req, res) => {
+      const { events } = req.body as { events: line.WebhookEvent[] };
+      console.dir(events);
+
+      for (const event of events) {
+        if (event.type === "message" && event.message.type === "text") {
+          lineClient
+            .replyMessage(event.replyToken, {
+              type: "text",
+              text: event.message.text.toUpperCase(),
+            })
+            .then(console.dir)
+            .catch(console.dir);
+        }
+      }
+
+      res.sendStatus(200);
+    }
+  );
+
+  app.listen(port, () => console.log(`running on port ${port}`));
+
+  return;
 
   const translate = new Translate({
     // https://github.com/googleapis/google-cloud-node/blob/main/docs/authentication.md
