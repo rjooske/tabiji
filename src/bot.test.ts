0a4811df45f8cd0f00e84fd844fac9ec009b7438
Ryosuke Takahashi <rjooske@gmail.com>
2023-03-11 18:30:00 +0900

    Tidy up string trimming tests

--- src/bot.test.ts
+++ src/bot.test.ts
@@ -1,7 +1,7 @@
 import { trimIfTooLong } from "./bot.js";
 
 describe("string trimming", () => {
-  describe("short enough; don't trim", () => {
+  it("short enough; don't trim", () => {
     const tests: [string, number][] = [
       ["aaa", 4],
       ["  hi  ", 10],
@@ -9,21 +9,19 @@ describe("string trimming", () => {
       ["⚠️👍🏻👍🏿", 3],
     ];
     for (const [s, n] of tests) {
-      it(`${JSON.stringify(s)}, ${n} -> ${JSON.stringify(s)}`, () =>
-        expect(trimIfTooLong(s, n)).toBe(s));
+      expect(trimIfTooLong(s, n)).toBe(s);
     }
   });
 
-  describe("too long; trim", () => {
+  it("too long; trim", () => {
     const tests: [string, number, string][] = [
       ["aaa aaa aaa", 4, "aaa…"],
       ["  hi  ", 2, " …"],
-      ["三文字以上", 4, "三文字…"],
+      ["三文字以上", 3, "三文…"],
       ["⚠️👍🏻👍🏿", 2, "⚠️…"],
     ];
     for (const [s, n, want] of tests) {
-      it(`${JSON.stringify(s)}, ${n} -> ${JSON.stringify(want)}`, () =>
-        expect(trimIfTooLong(s, n)).toBe(want));
+      expect(trimIfTooLong(s, n)).toBe(want);
     }
   });
 });
