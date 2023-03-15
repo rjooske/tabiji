d695f83acd8bb20a3cd378110decb649c226ff47
Ryosuke Takahashi <rjooske@gmail.com>
2023-03-15 18:07:16 +0900

    feat(bot): users can now choose which AI to use or cancel the request

--- src/bot.test.ts
+++ src/bot.test.ts
@@ -1,10 +1,24 @@
 import line from "@line/bot-sdk";
-import { exportedForTesting, StableDiffusionInJapaneseAction } from "./bot.js";
+import {
+  AnythingV4InJapaneseAction,
+  CancelAction,
+  ChooseAiOrCancelAction,
+  exportedForTesting,
+  InProgressWarningAction,
+  NonImmediateAction,
+  StableDiffusionInJapaneseAction,
+  TextTooLongWarningAction,
+} from "./bot.js";
 
-const { decideAction, trimIfTooLong } = exportedForTesting;
+const {
+  decideAction,
+  decideActionFromMessageEvent,
+  decideActionFromPostbackEvent,
+  trimIfTooLong,
+} = exportedForTesting;
 
 describe("decide actions", () => {
-  it("no actions for non message events", () => {
+  it("no actions for events other than message and postback", () => {
     const base: line.EventBase = {
       source: { type: "user", userId: "" },
       mode: "active",
@@ -23,7 +37,6 @@ describe("decide actions", () => {
         joined: { members: [] },
       },
       { ...base, type: "memberLeft", left: { members: [] } },
-      { ...base, type: "postback", replyToken: "", postback: { data: "" } },
       {
         ...base,
         type: "videoPlayComplete",
@@ -54,17 +67,26 @@ describe("decide actions", () => {
     }
   });
 
-  it("no actions for non single user messages", () => {
-    const base: Omit<line.MessageEvent, "source"> = {
+  it("no actions for non single user events", () => {
+    const messageEventBase: Omit<line.MessageEvent, "source"> = {
       type: "message",
       replyToken: "",
       message: { type: "text", text: "foo", id: "" },
       mode: "active",
       timestamp: 0,
     };
-    const tests: line.MessageEvent[] = [
-      { ...base, source: { type: "room", roomId: "" } },
-      { ...base, source: { type: "group", groupId: "" } },
+    const postbackBase: Omit<line.PostbackEvent, "source"> = {
+      type: "postback",
+      replyToken: "",
+      postback: { data: "" },
+      mode: "active",
+      timestamp: 0,
+    };
+    const tests: (line.MessageEvent | line.PostbackEvent)[] = [
+      { ...messageEventBase, source: { type: "room", roomId: "" } },
+      { ...messageEventBase, source: { type: "group", groupId: "" } },
+      { ...postbackBase, source: { type: "room", roomId: "" } },
+      { ...postbackBase, source: { type: "group", groupId: "" } },
     ];
     for (const test of tests) {
       expect(decideAction(test, new Map())).toBeUndefined();
@@ -103,7 +125,7 @@ describe("decide actions", () => {
         mode: "active",
         timestamp: 0,
       };
-      expect(decideAction(event, new Map())).toBeUndefined();
+      expect(decideActionFromMessageEvent(event, new Map())).toBeUndefined();
     }
   });
 
@@ -122,34 +144,34 @@ describe("decide actions", () => {
     }
   });
 
-  it("single user text messages are stable diffusion requests", () => {
+  it("ask which AI to use or cancel for single user text messages", () => {
     type Test = {
-      input: { userId: string; messageText: string };
-      want: StableDiffusionInJapaneseAction;
+      input: { replyToken: string; messageText: string };
+      want: ChooseAiOrCancelAction;
     };
     const tests: Test[] = [
       {
-        input: { userId: "123456", messageText: "あ" },
+        input: { replyToken: "123456", messageText: "あ" },
         want: {
-          type: "stable-diffusion-in-japanese",
-          initiatorLineUserId: "123456",
-          messageText: "あ",
+          type: "choose-ai-or-cancel",
+          replyToken: "123456",
+          prompt: "あ",
         },
       },
       {
-        input: { userId: "ABC", messageText: "　いろは　\n" },
+        input: { replyToken: "ABC", messageText: "　いろは　\n" },
         want: {
-          type: "stable-diffusion-in-japanese",
-          initiatorLineUserId: "ABC",
-          messageText: "いろは",
+          type: "choose-ai-or-cancel",
+          replyToken: "ABC",
+          prompt: "いろは",
         },
       },
     ];
     for (const test of tests) {
       const event: line.MessageEvent = {
         type: "message",
-        replyToken: "",
-        source: { type: "user", userId: test.input.userId },
+        replyToken: test.input.replyToken,
+        source: { type: "user", userId: "" },
         mode: "active",
         timestamp: 0,
         message: { type: "text", id: "", text: test.input.messageText },
@@ -158,8 +180,271 @@ describe("decide actions", () => {
     }
   });
 
-  // TODO: test cases where user sends a request while previous one is in progress
-  // TODO: test cases where the message text is too long
+  it("users can send only 1 request at a time", () => {
+    const base: line.MessageEvent = {
+      type: "message",
+      replyToken: "",
+      message: { type: "text", id: "", text: "てすと" },
+      mode: "active",
+      timestamp: 0,
+      source: { type: "user", userId: "" },
+    };
+    type Test = {
+      input: {
+        event: line.MessageEvent;
+        actionsInProgress: NonImmediateAction[];
+      };
+      want: ChooseAiOrCancelAction | InProgressWarningAction;
+    };
+    const tests: Test[] = [
+      {
+        input: {
+          event: {
+            ...base,
+            source: { type: "user", userId: "aaa" },
+            message: { type: "text", id: "", text: "かきくけこ" },
+            replyToken: "token",
+          },
+          actionsInProgress: [],
+        },
+        want: {
+          type: "choose-ai-or-cancel",
+          prompt: "かきくけこ",
+          replyToken: "token",
+        },
+      },
+      {
+        input: {
+          event: {
+            ...base,
+            source: { type: "user", userId: "user1" },
+            replyToken: "i am token",
+          },
+          actionsInProgress: [
+            {
+              type: "stable-diffusion-in-japanese",
+              initiatorLineUserId: "user1",
+              prompt: "あ",
+            },
+          ],
+        },
+        want: {
+          type: "in-progress-warning",
+          replyToken: "i am token",
+          actionInProgress: {
+            type: "stable-diffusion-in-japanese",
+            initiatorLineUserId: "user1",
+            prompt: "あ",
+          },
+        },
+      },
+      {
+        input: {
+          event: {
+            ...base,
+            source: { type: "user", userId: "I am a user" },
+            message: { type: "text", id: "", text: "お" },
+            replyToken: "TOUCAN",
+          },
+          actionsInProgress: [
+            {
+              type: "stable-diffusion-in-japanese",
+              initiatorLineUserId: "I am a different user",
+              prompt: "あ",
+            },
+          ],
+        },
+        want: {
+          type: "choose-ai-or-cancel",
+          replyToken: "TOUCAN",
+          prompt: "お",
+        },
+      },
+    ];
+    for (const { input, want } of tests) {
+      expect(
+        decideActionFromMessageEvent(
+          input.event,
+          new Map(
+            input.actionsInProgress.map((e) => [e.initiatorLineUserId, e])
+          )
+        )
+      ).toStrictEqual(want);
+    }
+  });
+
+  it("reject text messages that are too long", () => {
+    type Test = {
+      input: { replyToken: string; messageText: string };
+      want: TextTooLongWarningAction | ChooseAiOrCancelAction;
+    };
+    const tests: Test[] = [
+      {
+        input: { replyToken: "QWERTY", messageText: "\nshort enough\n" },
+        want: {
+          type: "choose-ai-or-cancel",
+          replyToken: "QWERTY",
+          prompt: "short enough",
+        },
+      },
+      {
+        input: {
+          replyToken: "ALMOST TOO LONG",
+          messageText: "　 \t" + "👍🏻".repeat(100) + "\t\r\n",
+        },
+        want: {
+          type: "choose-ai-or-cancel",
+          replyToken: "ALMOST TOO LONG",
+          prompt: "👍🏻".repeat(100),
+        },
+      },
+      {
+        input: {
+          replyToken: "too long by 1 character",
+          messageText: "　 \t" + "⚠️".repeat(101) + "\t\r\n",
+        },
+        want: {
+          type: "text-too-long-warning",
+          replyToken: "too long by 1 character",
+          maxLength: 100,
+        },
+      },
+      {
+        input: {
+          replyToken: "abc",
+          messageText: "\tDEFINITELY TOO LONG\n".repeat(100),
+        },
+        want: {
+          type: "text-too-long-warning",
+          replyToken: "abc",
+          maxLength: 100,
+        },
+      },
+    ];
+    for (const test of tests) {
+      const event: line.MessageEvent = {
+        type: "message",
+        replyToken: test.input.replyToken,
+        source: { type: "user", userId: "" },
+        mode: "active",
+        timestamp: 0,
+        message: { type: "text", id: "", text: test.input.messageText },
+      };
+      expect(decideAction(event, new Map())).toStrictEqual(test.want);
+    }
+  });
+
+  it("only respond to postback events with correct data", () => {
+    const base: line.PostbackEvent = {
+      type: "postback",
+      replyToken: "",
+      postback: { data: "" },
+      source: { type: "user", userId: "" },
+      mode: "active",
+      timestamp: 0,
+    };
+    type Test = {
+      input: line.PostbackEvent;
+      want:
+        | CancelAction
+        | StableDiffusionInJapaneseAction
+        | AnythingV4InJapaneseAction
+        | undefined;
+    };
+    const tests: Test[] = [
+      { input: { ...base, postback: { data: "" } }, want: undefined },
+      {
+        input: { ...base, postback: { data: "malformed json" } },
+        want: undefined,
+      },
+      {
+        input: {
+          ...base,
+          postback: {
+            data: JSON.stringify({ not_malformed: "but_wrong_shape" }),
+          },
+        },
+        want: undefined,
+      },
+      {
+        input: {
+          ...base,
+          postback: { data: JSON.stringify({ type: "some-unknown-type" }) },
+        },
+        want: undefined,
+      },
+      {
+        input: {
+          ...base,
+          postback: {
+            data: JSON.stringify({
+              type: "stable-diffusion",
+              this_payload_is_incomplete: true,
+            }),
+          },
+        },
+        want: undefined,
+      },
+      {
+        input: {
+          ...base,
+          postback: {
+            data: JSON.stringify({
+              type: "anything-v4",
+              is_bad_data: true,
+              is_my_fault: "hopefully-false",
+            }),
+          },
+        },
+        want: undefined,
+      },
+      {
+        input: {
+          ...base,
+          postback: { data: JSON.stringify({ type: "cancel" }) },
+          replyToken: "AAAA",
+        },
+        want: { type: "cancel", replyToken: "AAAA" },
+      },
+      {
+        input: {
+          ...base,
+          postback: {
+            data: JSON.stringify({
+              type: "stable-diffusion",
+              prompt: "テスト",
+            }),
+          },
+          source: { type: "user", userId: "IAmPlaceholder" },
+        },
+        want: {
+          type: "stable-diffusion-in-japanese",
+          initiatorLineUserId: "IAmPlaceholder",
+          prompt: "テスト",
+        },
+      },
+      {
+        input: {
+          ...base,
+          postback: {
+            data: JSON.stringify({
+              type: "anything-v4",
+              prompt: "いろはにほへと",
+            }),
+          },
+          source: { type: "user", userId: "schwa" },
+        },
+        want: {
+          type: "anything-v4-in-japanese",
+          initiatorLineUserId: "schwa",
+          prompt: "いろはにほへと",
+        },
+      },
+    ];
+    for (const { input, want } of tests) {
+      expect(decideActionFromPostbackEvent(input)).toStrictEqual(want);
+    }
+  });
 });
 
 describe("string trimming", () => {
--- src/bot.ts
+++ src/bot.ts
@@ -1,51 +1,80 @@
 import { Translate } from "@google-cloud/translate/build/src/v2/index.js";
 import line from "@line/bot-sdk";
 import stringz from "stringz";
+import { z } from "zod";
 import { ReplicateClient } from "./replicate.js";
+
+export type ChooseAiOrCancelAction = {
+  type: "choose-ai-or-cancel";
+  replyToken: string;
+  prompt: string;
+};
+
+export type CancelAction = { type: "cancel"; replyToken: string };
 
 export type StableDiffusionInJapaneseAction = {
   type: "stable-diffusion-in-japanese";
   initiatorLineUserId: string;
-  messageText: string;
+  prompt: string;
 };
 
-type TextTooLongWarningAction = {
+export type AnythingV4InJapaneseAction = {
+  type: "anything-v4-in-japanese";
+  initiatorLineUserId: string;
+  prompt: string;
+};
+
+export type TextTooLongWarningAction = {
   type: "text-too-long-warning";
   replyToken: string;
   maxLength: number;
 };
 
-type InProgressWarningAction = {
+export type InProgressWarningAction = {
   type: "in-progress-warning";
   replyToken: string;
   actionInProgress: NonImmediateAction;
 };
 
-type Action =
+export type Action =
+  | ChooseAiOrCancelAction
+  | CancelAction
   | StableDiffusionInJapaneseAction
+  | AnythingV4InJapaneseAction
   | TextTooLongWarningAction
   | InProgressWarningAction;
-type NonImmediateAction = StableDiffusionInJapaneseAction;
+export type NonImmediateAction =
+  | StableDiffusionInJapaneseAction
+  | AnythingV4InJapaneseAction;
 
-type ActionsInProgress = Map<string, NonImmediateAction>;
+export type ActionsInProgress = Map<string, NonImmediateAction>;
 
-const STABLE_DIFFUSION_PROMPT_MAX_LENGTH = 200;
+const STABLE_DIFFUSION_PROMPT_MAX_LENGTH = 100;
 
-function decideAction(
-  event: line.WebhookEvent,
+function decideActionFromMessageEvent(
+  event: line.MessageEvent,
   actions: ActionsInProgress
-): Action | undefined {
-  if (
-    event.source.type !== "user" ||
-    event.type !== "message" ||
-    event.message.type !== "text"
-  ) {
+):
+  | ChooseAiOrCancelAction
+  | TextTooLongWarningAction
+  | InProgressWarningAction
+  | undefined {
+  if (event.source.type !== "user" || event.message.type !== "text") {
     return;
   }
 
   const text = event.message.text.trim();
+  const textLength = stringz.length(text);
   if (text.length === 0) {
+    // Not sure if someone can send a text message that contains only
+    // whitespace characters but just in case
     return;
+  } else if (textLength > STABLE_DIFFUSION_PROMPT_MAX_LENGTH) {
+    return {
+      type: "text-too-long-warning",
+      replyToken: event.replyToken,
+      maxLength: STABLE_DIFFUSION_PROMPT_MAX_LENGTH,
+    };
   }
 
   const action = actions.get(event.source.userId);
@@ -56,18 +85,84 @@ function decideAction(
       actionInProgress: action,
     };
   }
-  if (stringz.length(text) > STABLE_DIFFUSION_PROMPT_MAX_LENGTH) {
-    return {
-      type: "text-too-long-warning",
-      replyToken: event.replyToken,
-      maxLength: STABLE_DIFFUSION_PROMPT_MAX_LENGTH,
-    };
-  }
+
   return {
-    type: "stable-diffusion-in-japanese",
-    initiatorLineUserId: event.source.userId,
-    messageText: text,
+    type: "choose-ai-or-cancel",
+    replyToken: event.replyToken,
+    prompt: text,
   };
+}
+
+const cancelPostbackActionSchema = z.object({ type: z.literal("cancel") });
+const stableDiffusionPostbackActionSchema = z.object({
+  type: z.literal("stable-diffusion"),
+  prompt: z.string().min(1),
+});
+const anythingV4PostbackActionSchema = z.object({
+  type: z.literal("anything-v4"),
+  prompt: z.string().min(1),
+});
+const postbackActionSchema = cancelPostbackActionSchema
+  .or(stableDiffusionPostbackActionSchema)
+  .or(anythingV4PostbackActionSchema);
+
+function parsePostbackData(s: string) {
+  try {
+    return postbackActionSchema.parse(JSON.parse(s));
+  } catch {
+    return;
+  }
+}
+
+function decideActionFromPostbackEvent(
+  event: line.PostbackEvent
+):
+  | CancelAction
+  | StableDiffusionInJapaneseAction
+  | AnythingV4InJapaneseAction
+  | undefined {
+  if (event.source.type !== "user") {
+    return;
+  }
+
+  const data = parsePostbackData(event.postback.data);
+  if (data === undefined) {
+    return;
+  }
+
+  switch (data.type) {
+    case "cancel":
+      return { type: "cancel", replyToken: event.replyToken };
+    case "stable-diffusion":
+      return {
+        type: "stable-diffusion-in-japanese",
+        initiatorLineUserId: event.source.userId,
+        prompt: data.prompt,
+      };
+    case "anything-v4":
+      return {
+        type: "anything-v4-in-japanese",
+        initiatorLineUserId: event.source.userId,
+        prompt: data.prompt,
+      };
+  }
+}
+
+function decideAction(
+  event: line.WebhookEvent,
+  actions: ActionsInProgress
+): Action | undefined {
+  if (event.source.type !== "user") {
+    return;
+  }
+  switch (event.type) {
+    case "message":
+      return decideActionFromMessageEvent(event, actions);
+    case "postback":
+      return decideActionFromPostbackEvent(event);
+    default:
+      return;
+  }
 }
 
 function trimIfTooLong(s: string, maxLength: number) {
@@ -91,6 +186,64 @@ export class Bot {
     private readonly replicateClient: ReplicateClient
   ) {}
 
+  private async handleChooseAiOrCancelAction(action: ChooseAiOrCancelAction) {
+    const promptPreview = trimIfTooLong(action.prompt, 30);
+    const replyText = `
+🖌️ 「${promptPreview}」を生成します！使用するAIを選択してください
+• Stable Diffusion: 様々なスタイルに対応した汎用AIです
+• Anything V4: アニメ風の画像を作るのが得意です
+`.trim();
+    await this.lineClient.replyMessage(action.replyToken, {
+      type: "text",
+      text: replyText,
+      // TODO: hmmmmm
+      quickReply: {
+        items: [
+          {
+            type: "action",
+            action: {
+              type: "postback",
+              label: "キャンセル",
+              displayText: "キャンセル",
+              data: JSON.stringify({ type: "cancel" }),
+            },
+          },
+          {
+            type: "action",
+            action: {
+              type: "postback",
+              label: "Stable Diffusion",
+              displayText: "Stable Diffusion",
+              data: JSON.stringify({
+                type: "stable-diffusion",
+                prompt: action.prompt,
+              }),
+            },
+          },
+          {
+            type: "action",
+            action: {
+              type: "postback",
+              label: "Anything V4",
+              displayText: "Anything V4",
+              data: JSON.stringify({
+                type: "anything-v4",
+                prompt: action.prompt,
+              }),
+            },
+          },
+        ],
+      },
+    });
+  }
+
+  private async handleCancelAction(action: CancelAction) {
+    await this.lineClient.replyMessage(action.replyToken, {
+      type: "text",
+      text: "✅ キャンセルしました！",
+    });
+  }
+
   private async handleStableDiffusionInJapaneseAction(
     action: StableDiffusionInJapaneseAction
   ) {
@@ -99,11 +252,38 @@ export class Bot {
       type: "text",
       text: "🎨 生成中…",
     });
-    const [en] = await this.translateClient.translate(action.messageText, {
+    const [en] = await this.translateClient.translate(action.prompt, {
       from: LANGUAGE_CODE_JAPANESE,
       to: LANGUAGE_CODE_ENGLISH,
     });
-    console.log(`${action.messageText} → ${en}`); // TODO
+    console.log(`${action.prompt} → ${en}`); // TODO
+    const urls = await this.replicateClient.callStableDiffusion(en);
+    await this.lineClient.pushMessage(action.initiatorLineUserId, [
+      { type: "text", text: "🖼️ 完成！" },
+      ...urls.map(
+        (url): line.ImageMessage => ({
+          type: "image",
+          previewImageUrl: url,
+          originalContentUrl: url,
+        })
+      ),
+    ]);
+    this.actionsInProgress.delete(action.initiatorLineUserId);
+  }
+
+  private async handleAnythingV4InJapaneseAction(
+    action: AnythingV4InJapaneseAction
+  ) {
+    this.actionsInProgress.set(action.initiatorLineUserId, action);
+    await this.lineClient.pushMessage(action.initiatorLineUserId, {
+      type: "text",
+      text: "🎨 生成中…",
+    });
+    const [en] = await this.translateClient.translate(action.prompt, {
+      from: LANGUAGE_CODE_JAPANESE,
+      to: LANGUAGE_CODE_ENGLISH,
+    });
+    console.log(`${action.prompt} → ${en}`); // TODO
     const urls = await this.replicateClient.callStableDiffusion(en);
     await this.lineClient.pushMessage(action.initiatorLineUserId, [
       { type: "text", text: "🖼️ 完成！" },
@@ -128,7 +308,7 @@ export class Bot {
   }
 
   private async handleInProgressWarningAction(action: InProgressWarningAction) {
-    const prompt = trimIfTooLong(action.actionInProgress.messageText, 30);
+    const prompt = trimIfTooLong(action.actionInProgress.prompt, 30);
     await this.lineClient.replyMessage(action.replyToken, {
       type: "text",
       text: `⚠️ 「${prompt}」を生成中です！`,
@@ -141,8 +321,14 @@ export class Bot {
       return;
     }
     switch (action.type) {
+      case "choose-ai-or-cancel":
+        return await this.handleChooseAiOrCancelAction(action);
+      case "cancel":
+        return await this.handleCancelAction(action);
       case "stable-diffusion-in-japanese":
         return await this.handleStableDiffusionInJapaneseAction(action);
+      case "anything-v4-in-japanese":
+        return await this.handleAnythingV4InJapaneseAction(action);
       case "in-progress-warning":
         return await this.handleInProgressWarningAction(action);
       case "text-too-long-warning":
@@ -151,4 +337,9 @@ export class Bot {
   }
 }
 
-export const exportedForTesting = { decideAction, trimIfTooLong };
+export const exportedForTesting = {
+  decideAction,
+  decideActionFromMessageEvent,
+  decideActionFromPostbackEvent,
+  trimIfTooLong,
+};
