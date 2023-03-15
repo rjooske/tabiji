79d865928f021102c5f7dccdc2928e7703e2ed64
Ryosuke Takahashi <rjooske@gmail.com>
2023-03-15 18:05:25 +0900

    feat(replicate): client can now call anything v4

--- src/replicate.ts
+++ src/replicate.ts
@@ -2,6 +2,7 @@ import { predict } from "replicate-api";
 import { z } from "zod";
 
 const stableDiffusionOutputSchema = z.array(z.string().url());
+const anythingV4OutputSchema = z.array(z.string().url());
 
 export class ReplicateClient {
   constructor(private readonly token: string) {}
@@ -14,5 +15,15 @@ export class ReplicateClient {
       poll: true,
     });
     return stableDiffusionOutputSchema.parse(output);
+  }
+
+  async callAnythingV4(prompt: string) {
+    const { output } = await predict({
+      token: this.token,
+      model: "cjwbw/anything-v4.0",
+      input: { prompt, num_outputs: 4 },
+      poll: true,
+    });
+    return anythingV4OutputSchema.parse(output);
   }
 }
